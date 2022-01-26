66f310c874eb11f511592d102171e48ae6ecf843
spectan <sammysosa95@gmail.com>
2022-01-26 12:02:54 -0500

    Changed theme

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -4,5 +4,5 @@ import dash_bootstrap_components as dbc
 #This will solely contain the information needed to run the dashboard instance and nothing more for cleanliness and avoiding circular imports.
 #Run the dashboard instance from index.py!
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
