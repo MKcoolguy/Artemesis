db3448869220ee3cf893a19dea5efdc213c41134
spectan <sammysosa95@gmail.com>
2022-03-07 09:03:25 -0500

    Segmented graphs into their own file.

---
+++ Dashboard/figures.py
@@ -0,0 +1,49 @@
+from dash import html
+from dash import dcc
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import os
+cwd = os.path.dirname(__file__)  # Used for consistent file detection.
+
+
+class Graphs:
+    #Creates the temperature graph
+    def createTempGraph():
+        df = pd.read_csv(os.path.join(cwd, 'assets/data/temperature.csv'))
+        df.columns = ['Time', 'Temperature']
+        return go.Figure(
+            data=px.line(
+                x=df['Time'],
+                y=df['Temperature']
+            ).update_layout(
+                xaxis_title='Time',
+                yaxis_title='Temperature',
+                template='plotly_dark',
+                plot_bgcolor='rgba(0,0,0,0)',
+                paper_bgcolor='rgba(0,0,0,0)',
+            )
+        )
+    
+        # Iris bar figure
+    def drawFigure():
+        df = px.data.iris()
+        return html.Div([
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.Graph(
+                        figure=px.bar(
+                             df, x="sepal_width", y="sepal_length", color="species"
+                             ).update_layout(
+                            template='plotly_dark',
+                            plot_bgcolor='rgba(0, 0, 0, 0)',
+                            paper_bgcolor='rgba(0, 0, 0, 0)',
+                        ),
+                        config={
+                            'displayModeBar': False
+                        }
+                    )
+                ])
+            ),
+        ])
