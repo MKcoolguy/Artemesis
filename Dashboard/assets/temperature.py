d08ad813bdb478fb7a49b6f6df62176212abeb15
Sam <sam@samuel.zone>
2022-03-07 05:59:39 -0500

    Fixed csv formatting

--- Dashboard/assets/temperature.py
+++ Dashboard/assets/temperature.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
 import time
 from time import sleep
 import board
@@ -8,54 +7,48 @@ import adafruit_dht
 import csv
 import datetime
 import os.path
-# Initial the dht device, with data pin connected to:
+# Initialize the dht device, with data pin connected to: 21
 dhtDevice = adafruit_dht.DHT11(board.D21)
+#Datafile Location
+datafile = "/home/pi/Artemis/Dashboard/assets/data/temperature.csv"
 
-datafile = "/home/pi/Artemis/Dashboard/assets/data/TempHumid.csv"
-#open csv file and append temp data to it
-#f = open('')
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
-
-#get temp and humidity and add to 'data' dictionary
+#Fetch temperature, time, and humidity
 def get_data():
     now = datetime.datetime.now()
-    temperature = dhtDevice.temperature * (9 / 5) - 32
+    temperature = dhtDevice.temperature * 1.8 + 32
     humidity = dhtDevice.humidity
     if humidity is not None and temperature is not None:
-        data = {'timestamp':str(now.strftime("%Y%m%d_%H-%M-%S")), 'temperature':temperature, 'humidity':humidity}
+        data = {'timestamp': str(now.strftime(
+            "%Y%m%d_%H-%M-%S")), 'temperature': temperature}
         print(data)
         return(data)
     else:
-        data = {'timestamp':None, 'temperature':None, 'humidity': None}
+        data = {'timestamp': None, 'temperature': None, 'humidity': None}
         print(data)
         return(data)
 
-#write temp, humidity and time stamps to csv file
+#Writes Temperature and Time to .csv file
 def add_to_file(data):
-    if os.path.isfile(datafile): #checks if file exists. if yes, appends values for dictionary under corresponding header in a new line
-        with open(datafile, 'a', newline='') as csvfile: 
-            fieldnames = ['Time', 'Temperature', 'Humidity']
-            data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    # checks if file exists. if yes, appends values for dictionary under corresponding header in a new line
+    if os.path.isfile(datafile):
+        with open(datafile, 'a', newline='') as csvfile:
+            fieldnames = ['Time', 'Temperature']
+            data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            data_writer.writerow(
+                {'Time': data['timestamp'], 'Temperature': data['temperature']})
 
-            data_writer.writerow({'Time': data['timestamp'], 'Temperature': data['temperature'], 'Humidity': data['humidity']})
-    
-    else: #creates file (that has been checked and does not yet exist) and adds headers and values for all 3 keys in dict
-        with open(datafile, 'w', newline='') as csvfile: 
-            fieldnames = ['Time', 'Temperature', 'Humidity']
-            data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
+    else:  # creates file (that has been checked and does not yet exist) and adds headers and values for all 3 keys in dict
+        with open(datafile, 'w', newline='') as csvfile:
+            fieldnames = ['Time', 'Temperature']
+            data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             data_writer.writeheader()
-            data_writer.writerow({'Time': data['timestamp'], 'Temperature': data['temperature'], 'Humidity': data['humidity']})
+            data_writer.writerow(
+                {'Time': data['timestamp'], 'Temperature': data['temperature']})
 
-
-
+#While script is active
 while True:
     try:
-        # Print the values to the serial port
+        #Prints the values of the sensor to the terminal
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
@@ -64,6 +57,7 @@ while True:
                 temperature_f, temperature_c, humidity
             )
         )
+        #Fetches the data to write it to the file
         data = get_data()
         if data is not None:
             add_to_file(data)
