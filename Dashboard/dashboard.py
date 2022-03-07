696f4f80a208f18a0dd87545180c4c1fa6680f3f
spectan <sammysosa95@gmail.com>
2022-03-07 09:05:45 -0500

    No longer used

--- Dashboard/dashboard.py
+++
@@ -1,8 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-#This will solely contain the information needed to run the dashboard instance and nothing more for cleanliness and avoiding circular imports.
-#Run the dashboard instance from index.py!
-
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
