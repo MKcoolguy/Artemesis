292e5347617b7e61eb1af7ad135f7e49f5de5612
Matteo <matteokitic7@gmail.com>
2022-03-26 17:47:25 -0400

    trying another solution

--- Dashboard/assets/app_utils.py
+++ Dashboard/assets/app_utils.py
@@ -14,16 +14,18 @@ class Temperature:
     # SPDX-License-Identifier: MI
     # Initialize the dht device, with data pin connected to: 21
 
+    global dhtDevice
+    global datafile
     dhtDevice = adafruit_dht.DHT11(board.D21)
     # Datafile Location
     datafile = "/home/pi/Artemis/Dashboard/assets/data/temperature.csv"
 
 
     # Fetch temperature, time, and humidity
-    def get_data(self):
+    def get_data():
         now = datetime.datetime.now()
-        temperature = self.dhtDevice.temperature * 1.8 + 32
-        humidity = self.dhtDevice.humidity
+        temperature = dhtDevice.temperature * 1.8 + 32
+        humidity = dhtDevice.humidity
         if humidity is not None and temperature is not None:
             data = {'timestamp': str(now.strftime(
                 "%Y%m%d_%H-%M-%S")), 'temperature': temperature}
@@ -36,17 +38,17 @@ class Temperature:
 
 
     # Writes Temperature and Time to .csv file
-    def add_to_file(self, data):
+    def add_to_file(data):
         # checks if file exists. if yes, appends values for dictionary under corresponding header in a new line
-        if os.path.isfile(self.datafile):
-            with open(self.datafile, 'a', newline='') as csvfile:
+        if os.path.isfile(datafile):
+            with open(datafile, 'a', newline='') as csvfile:
                 fieldnames = ['Time', 'Temperature']
                 data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 data_writer.writerow(
                     {'Time': data['timestamp'], 'Temperature': data['temperature']})
 
         else:  # creates file (that has been checked and does not yet exist) and adds headers and values for all 3 keys in dict
-            with open(self.datafile, 'w', newline='') as csvfile:
+            with open(datafile, 'w', newline='') as csvfile:
                 fieldnames = ['Time', 'Temperature']
                 data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 data_writer.writeheader()
