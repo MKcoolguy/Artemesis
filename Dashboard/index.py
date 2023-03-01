e5c60a3b2ca5b2939924c5fced0b7ab26a336e55
Jacob Franscoviak <jfranscoviak@ggc.edu>
2023-03-01 08:51:18 -0500

    Adds server hosting functionality and displays the correct IP

--- Dashboard/index.py
+++ Dashboard/index.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import socket
 
 import cv2
 import dash
@@ -271,8 +272,10 @@ def display_page(pathname):
     elif pathname == '/archivedData':
         return archived_data_page
 
+##Grabbing local ip
+address = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
 #Run application
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host=address, port=8050)
     
