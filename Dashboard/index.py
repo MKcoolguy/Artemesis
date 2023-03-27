0b1d718e662a9e25d0837d097a004c4fe9b5b316
Bradley Nguyen <Bnguyen11@ggc.edu>
2023-03-27 11:42:20 -0400

    made color changes to the UI, added a logo to the navigation bar. -- there needed some changes on the width and height of the logo

--- Dashboard/index.py
+++ Dashboard/index.py
@@ -40,7 +40,7 @@ def video_feed():
 
 
 # Base layout for all webpages
-logo = html.Img(src='https://colleges-static.raise.me/georgia-gwinnett-college/logo-120x120.png', style={'height': '100%', 'width': '100%'})
+logo = html.Img(src='https://colleges-static.raise.me/georgia-gwinnett-college/logo-120x120.png', style={'height': '80%', 'width': '80'})
 
 app.layout = html.Div([
     dbc.NavbarSimple(
