4bb281949ef84dcb031c9f1cc7e768d8f021693a
Bradley Nguyen <Bnguyen11@ggc.edu>
2023-04-01 23:21:37 -0400

    edited, didn't change much, just looking over it.

--- Dashboard/UserCreation.py
+++ Dashboard/UserCreation.py
@@ -1,6 +1,6 @@
 AuthenticatedUsers = open("Users.txt", "a")
 userName = input('What is the username the user would like to use?\n')
-passWord = input('WHat is the password the user would like to use?\n')
+passWord = input('What is the password the user would like to use?\n')
 
 AuthenticatedUsers.writelines(userName)
 AuthenticatedUsers.writelines(passWord)
